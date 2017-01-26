9163bf019e370817f7ed8e7542c7c98367f7dc92
Oliver Steele <steele@osteele.com>
2017-01-26 09:04:35 -0500

    multiple images per entry; outputs in build dir

--- gallery.py
+++ gallery.py
@@ -6,7 +6,9 @@ import mistune
 from github import Github  # , GithubException
 from jinja2 import Environment
 
+BUILD_DIR = './build'
 GH_TOKEN = os.environ['GITHUB_API_TOKEN']
+
 origin_repo_name = 'olinlibrary/htl-lab1'
 
 gh = Github(GH_TOKEN)
@@ -42,7 +44,7 @@ assert not set(more_gitub_logins) - set(repo.owner.login for repo in repos)
 
 repo_readmes = [get_repo_content_text(repo, 'README.md') for repo in repos]
 repo_readme_image_urls = [get_markdown_image_urls(readme) for readme in repo_readmes]
-owner_image_dict = {repo.owner.login: repo.get_contents(urls[0])
+owner_image_dict = {repo.owner.login: [repo.get_contents(re.sub(r'\.\/', '', u)) for u in urls]
                     for repo, urls in zip(repos, repo_readme_image_urls)
                     if urls}
 
@@ -51,21 +53,23 @@ owner_image_dict = {repo.owner.login: repo.get_contents(urls[0])
 #
 
 
-def local_image_path_for(repo_owner, image_path):
-    return os.path.join('images', owner + os.path.splitext(image_path)[1])
+def local_image_path_for(owner, image_path):
+    return os.path.join('images', owner, os.path.split(image_path)[1])
 
 
-os.makedirs('images', exist_ok=True)
-for owner, image in owner_image_dict.items():
-    with open(local_image_path_for(owner, image.path), 'wb') as f:
-        f.write(base64.b64decode(repos[0].get_git_blob(image.sha).content))
+for owner, images in owner_image_dict.items():
+    os.makedirs(os.path.join(BUILD_DIR, 'images', owner), exist_ok=True)
+    for image in images:
+        with open(os.path.join(BUILD_DIR, local_image_path_for(owner, image.path)), 'wb') as f:
+            f.write(base64.b64decode(repos[0].get_git_blob(image.sha).content))
 
+# %%
 env = Environment()
 tpl = env.from_string(open('gallery.tmpl.html').read())
 
-output_path = 'gallery.html'
+output_path = os.path.join(BUILD_DIR, 'gallery.html')
 with open(output_path, 'w') as f:
-    entries = [{'title': login, 'image_src': local_image_path_for(owner, image.path)}
-               for login, image in owner_image_dict.items()]
+    entries = [{'title': owner, 'image_srcs': [local_image_path_for(owner, image.path) for image in images]}
+               for owner, images in owner_image_dict.items()]
     f.write(tpl.render(entries=entries))
 print('wrote', len(owner_image_dict), 'slides to', output_path)
